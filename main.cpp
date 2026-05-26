edb419be0ad82aa4aa308bae04dff066320b608d
GUSTAVO FERREIRA PEDROSO <03713982074@senacrs.edu.br>
2026-05-26 19:35:53 -0300

    interpolaçao de variaveis

--- main.cpp
+++ main.cpp
@@ -1,23 +1,13 @@
 #include <iostream>
+#include <format>
+#include <string>
 
 int main (){
 
-int numero;
-int contador = 0;
+std::string nome = "gustavo";
 
-while(true){
-  std::cout<<"Digite um numero (0 pra parar): ";
-  std::cin>>numero;
-
-    if(numero == 0){
-          break;
-    }
-
-      contador++;
-}
-
-std::cout<<"Voce digitou "<<contador<<" Numeros!"<<std::endl;
-
+std::string mensagem = std::format("ola, {}! voce tem {} anos\n, nome")
+std::cout<<mensagem;
 
 return 0;
   }
