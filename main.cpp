4ac24869bef34fd8b774ea84557353d6b4c9948a
GUSTAVO FERREIRA PEDROSO <03713982074@senacrs.edu.br>
2026-05-26 21:36:24 -0300

    definir maior numero arr

--- main.cpp
+++ main.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
 
+
+
+int encontrarMaior(int arr[], int tamanho){
+int maior = arr[0];
+
+for(int i = 1; i < tamanho; i++){
+      if(arr[1] > maior){
+          maior = arr[1];
+      }
+}
+      return maior;
+}
+
 int main() {
 
 int numeros[5];
-int soma = 0;
 
-for(int i = 0; i < 5; i++){
-  std::cout<<"Digite o numero: "<<(i + 1)<<": ";
-  std::cin>>numeros[i];
-  soma += numeros[i];
-}
+    std::cout<<"Digite 5 numeros: "<<std::endl;
+        for(int i = 0; i < 5; i++){
+            std::cout<<"Digite o numero "<<(i + 1)<<": ";
+            std::cin>>numeros[i];
+        }
 
-std::cout<<"Soma: "<<soma<<std::endl;
+        std::cout<<"maior numero: "<<encontrarMaior(numeros, 5)<<std::endl;
+
 
     return 0;
 }
