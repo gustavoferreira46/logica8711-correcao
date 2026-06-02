b8bf4110714132319016b9f3ae5213efbbf85aa6
GUSTAVO FERREIRA PEDROSO <03713982074@senacrs.edu.br>
2026-06-02 20:12:52 -0300

    bool telower palindromo

--- main.cpp
+++ main.cpp
@@ -1,29 +1,34 @@
 #include <iostream>
 #include <string>
 #include <cctype>
-bool ehpalindromo(std:: string s){
-    int inicio = 0;
-    int fim = s.length() - 1;
 
-    while(inicio < fim){
-    if(s[inicio] != s [fim]){
+bool ehpalindromo(std::string s, int inicio, int fim){
+    if (inicio >= fim){
+        return true;
+    }
+    if(s[inicio] != s[fim]){
             return false;
     }
-    inicio ++;
-    fim--;
-    }
-    return true;
+    return ehpalindromo(s, inicio + 1, fim -1);
 }
 
+
 int main(){
-    std::string palavra;
+
+std::string palavra;
+
+  
     std::cout<<"Digite uma palavra: "<<std::endl;
     std::cin>>palavra;
-    for( int i = 0; i< palavra.length(); i++){
-            palavra[i] = std::tolower(palavra[i]);
+    
+    
+    for(char &c : palavra){
+        c = std::tolower(c);
     }
+         
+    
 
-    if(ehpalindromo(palavra)){
+    if(ehpalindromo(palavra, 0, palavra.length() - 1)){
         std::cout<<"E palindromo!"<<std::endl;
     }else{
             std::cout<<"nao e palindromo"<<std::endl;
