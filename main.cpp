31307c68d0a30c89965db3842f48c207fa9cb67b
GUSTAVO FERREIRA PEDROSO <03713982074@senacrs.edu.br>
2026-06-02 21:34:55 -0300

    struct aluno calcular nota

--- main.cpp
+++ main.cpp
@@ -1,21 +1,29 @@
 #include <iostream>
 #include <string>
 
-struct pessoa{
+struct aluno {
     std::string nome;
-    int idade;
-    float altura;
+    float nota1;
+    float nota2;
 };
+
+float calcularMedia(aluno a){
+        return(a.nota1 + a.nota2) / 2;
+}
 
 int main(){
 
-pessoa p1 {"junior", 20, 1.80};
-pessoa p2 {"carlos", 19, 1.70};
+ aluno a1;
 
-std::cout<<p1.nome<<" tem "<<p1.idade<<" anos e mede "<<p1.altura<<" m "<<std::endl;
-std::cout<<p2.nome<<" tem "<<p2.idade<<" anos e mede "<<p2.altura<<"m ";
+std::cout<<"Digite sua primeira nota: "<<std::endl;
+std::cin>>a1.nota1;
+    std::cout<<"Digite sua segunda nota: "<<std::endl;
+    std::cin>>a1.nota2;
 
+
+    float media = calcularMedia(a1);
+
+    std::cout<<a1.nome<<" - Media: "<<media<<std::endl;
 
     return 0;
 }
-
