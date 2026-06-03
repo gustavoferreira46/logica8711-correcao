0ff7657bfdbb57b8db05d76a2bd4bdf9ccd78414
GUSTAVO FERREIRA PEDROSO <03713982074@senacrs.edu.br>
2026-06-02 21:47:43 -0300

    struct aluno mostrar nota

--- main.cpp
+++ main.cpp
@@ -1,29 +1,21 @@
 #include <iostream>
 #include <string>
 
-struct aluno {
+struct Aluno{
     std::string nome;
-    float nota1;
-    float nota2;
+    float nota;
 };
-
-float calcularMedia(aluno a){
-        return(a.nota1 + a.nota2) / 2;
-}
-
 int main(){
 
- aluno a1;
+    Aluno alunos[3]={
+        {"Henrique", 8.5},
+        {"Paula", 9.5},
+        {"Juliana", 4.5}
+    };
 
-std::cout<<"Digite sua primeira nota: "<<std::endl;
-std::cin>>a1.nota1;
-    std::cout<<"Digite sua segunda nota: "<<std::endl;
-    std::cin>>a1.nota2;
-
-
-    float media = calcularMedia(a1);
-
-    std::cout<<a1.nome<<" - Media: "<<media<<std::endl;
+    for(int i = 0; i < 3; i++){
+        std::cout<<alunos[i].nome<<": "<<alunos[i].nota<<std::endl;
+    }
 
     return 0;
 }
