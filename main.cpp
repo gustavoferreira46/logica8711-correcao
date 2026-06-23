3798191c6df596810d4005b81fa436027d499cdd
GUSTAVO FERREIRA PEDROSO <03713982074@senacrs.edu.br>
2026-06-23 20:49:38 -0300

    descobrir numeros pares e impares

--- main.cpp
+++ main.cpp
@@ -2,63 +2,28 @@
 
 
 int main(){
+ 
+int numeros[] = {1,2,3,4,5,6,7,8,9,10};
+int tamanho = 10;
 
-        int fila [10];
-        int fim = -1;
-        int inicio;
-        int opcao;
+std::cout<<" === PARES E IMPARES === "<<std::endl;
+std::cout<<std::endl;
 
-        std::cout<<"=== FILA COM MENU ==="<<std::endl;
-
-        while (true){
-                std::cout<<"Escolha uma opção:"<<std::endl;
-                std::cout<<"1 - enfileirar"<<std::endl;
-                std::cout<<"2 - desenfileirar"<<std::endl;
-                std::cout<<"3 - exibir fila"<<std::endl;
-                std::cout<<"4 - Sair"<<std::endl;
-                std::cout<<"Escolha: ";
-                std::cin>>opcao;
-
-                if(opcao == 1){
-                        if(fim < 10){
-                                int valor;
-                                std::cout<<"Digite o valor"<<std::endl;
-                                std::cin>>valor;
-                                fim++;
-                                std::cout<<"Valor enfileirado com sucesso!"<<std::endl;
-                        }else{
-                                std::cout<<"fila cheia!"<<std::endl;
-                        }
-                }
-          else if(opcao == 2){
-        if(inicio < fim){
-            std::cout << "Removido: " << fila[inicio] << std::endl;
-            inicio++;
-        } else {
-            std::cout << "Fila vazia! Nao ha o que remover." << std::endl;
+std::cout<<"Pares: ";
+for(int i = 0; i < tamanho; i++){
+        if(numeros[i] % 2 == 0){
+                std::cout<<numeros[i]<<" ";
         }
-    }
-    else if(opcao == 3){
-        if(inicio < fim){
-            std::cout << "fila: ";
-            for(int i = inicio; i < fim; i++){
-               std::cout<<fila[i]<<" ";
-            }
-            std::cout<<std::endl;
-        } else {
-            std::cout << "fila vazia!" << std::endl;
-        }
-    }
-    else if(opcao == 4){
-        
-        std::cout << "Saindo..." << std::endl;
-        break;
-    }
-    else {
-      
-        std::cout << "Opcao invalida!" << std::endl;
-    }
 }
+std::cout<<std::endl;
+std::cout<<"impares: ";
+for(int i = 0; i < tamanho; i++){
+        if(numeros[i] % 2 != 0){
+                std::cout<<numeros[i]<<" ";
+        }
+        }
 return 0;
 }
+
+
 
