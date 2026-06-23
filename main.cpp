aded67489144f0b29d633d4fb69dc660576a7415
GUSTAVO FERREIRA PEDROSO <03713982074@senacrs.edu.br>
2026-06-23 19:15:35 -0300

    empilhando e desempilhando

--- main.cpp
+++ main.cpp
@@ -1,32 +1,26 @@
 #include<iostream>      
-#include<string>
 
-struct Convocados {
-        std::string nome;
-        int numero;
-        std::string posicao;
-};
 
 int main(){
-        Convocados convocados [3];
-        std::cout<<"==== CONVOCAOO DA SELECAO ===="<<std::endl;
+        int pilha [5];
+        int topo = -1;
+        int tamanho = 5;
+
+        std::cout<<"==== PILHA ===="<<std::endl;
         std::cout<<std::endl;
 
-        for(int i = 0; i < 3; i++){
-                std::cout<<"Jogador "<<(i + 1 )<<": "<<std::endl;
-                std::cout<<"Nome: "<<std::endl;
-                std::cin>>convocados[i].nome;
-                std::cout<<"Numero: ";
-                std::cin>>convocados[i].numero;
-                std::cout<<"Posicao: ";
-                std::cin>>convocados[i].posicao;
-        }
+        pilha[0] = 10;
+        pilha[1] = 20;
+        pilha[2] = 30;
+        topo = 2;
 
-        std::cout<<"===== JOGADORES CONVOCADOS ====="<<std::endl;
+        std::cout<<"elementos empilhados: 10, 20, 30"<<std::endl;
         std::cout<<std::endl;
 
-        for(int i = 0; i < 3; i++){
-                std::cout<<convocados[i].numero<<" - "<<convocados[i].nome<<" ("<<convocados[i].posicao<<")"<<std::endl; 
+        std::cout<<"Desempilhados..."<<std::endl;
+        while(topo >= 0){
+                std::cout<<"Removida: "<<pilha[topo]<<std::endl;
+                topo--;
         }
         return 0;
 }
