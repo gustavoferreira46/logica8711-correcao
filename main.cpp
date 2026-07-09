52e0ece7ae96cf0fed9cf0935825b02840750d02
GUSTAVO FERREIRA PEDROSO <03713982074@senacrs.edu.br>
2026-07-09 20:05:28 -0300

    Adiciona programa para identificar a primeira e a última letra de uma palavra

--- main.cpp
+++ main.cpp
@@ -1,25 +1,53 @@
-#include <iostream>
+#include <iostream> 
 
+#include <string> 
 
+ 
 
-    int main () {
+ 
 
-        int idade;
+    int main () { 
 
-        std::cout<<"Digite sua idade para verificar se voce pode se cadastrar: ";
-        std::cin>>idade;
+ 
 
-        if (idade >= 18 && idade < 60) {
-            std::cout<<"Maior de idade, prosseguindo cadastro!"<<std::endl;
+        std::string palavra; 
 
-        }else if (idade >= 60 ){
-        
-                std::cout<<"terceira idade!!"<<std::endl;            
-        
-        }else{
-            std::cout<<"Menor de idade!!  "<<std::endl;
-        }
-        
+        std::string Pletra; 
 
-        return 0;
-    }
+        std::string Uletra; 
+
+ 
+
+        std::cout << "Digite uma palavra: "; 
+
+        std::cin >> palavra; 
+
+ 
+
+        std::cout<<"a palavra digitada foi: "<<palavra<<std::endl; 
+
+ 
+
+        if (palavra.length() > 0) { 
+
+            Pletra = palavra[0]; 
+
+            std::cout << "A primeira letra da palavra e: " << Pletra << std::endl; 
+
+        } 
+
+ 
+
+        if (palavra.length() > 0) { 
+
+            Uletra = palavra[palavra.length() - 1]; 
+
+            std::cout << "A ultima letra da palavra e: " << Uletra << std::endl; 
+
+        } 
+
+ 
+
+        return 0; 
+
+    } 
