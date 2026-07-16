245adfe6659faec9b69a9aa318218cec534fc55e
GUSTAVO FERREIRA PEDROSO <03713982074@senacrs.edu.br>
2026-07-16 19:45:35 -0300

    ler 5 numeros e exibir qual o maior

--- main.cpp
+++ main.cpp
@@ -1,30 +1,17 @@
 #include <iostream>
 
-bool ehPrimo(int numero){
-    if(numero <= 1){
-        return false;
+int encontrarMaior (int* arr, int tamanho){
+    int maior = arr[0];
+    for (int i = 1; i < tamanho; i++)
+    if (arr[i] > maior){
+        maior = arr[i];
     }
-
-    for(int i = 2; i < numero; i++){
-        if(numero % i == 0){
-            return false;
-        }
-    }
-    return true;
+    return maior;
 }
-
-int main(){
-    int numero;
-
-    std::cout<<"Digite um numero: "<<std::endl;
-    std::cin>>numero;
-
-    if(ehPrimo(numero)){
-        std::cout<<"e primo! "<<std::endl;
-    }else{
-        std::cout<<"nao e primo! "<<std::endl;
-    }
     
-
+int main(){
+    int arr[5] = {77,12,45,87,43};
+    std::cout<<"o maior numero e "<<encontrarMaior(arr, 5)<<std::endl;
+    
     return 0;
 }
