9afdefc6c8d9df22520118eb69b671c29c19afd0
GUSTAVO FERREIRA PEDROSO <03713982074@senacrs.edu.br>
2026-07-16 20:03:05 -0300

    ler 5 numeros e exibir a media

--- main.cpp
+++ main.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 
-int encontrarMaior (int* arr, int tamanho){
-    int maior = arr[0];
-    for (int i = 1; i < tamanho; i++)
-    if (arr[i] > maior){
-        maior = arr[i];
-    }
-    return maior;
-}
+
     
 int main(){
-    int arr[5] = {77,12,45,87,43};
-    std::cout<<"o maior numero e "<<encontrarMaior(arr, 5)<<std::endl;
-    
+    int ingressos[5] = {6000, 8000, 10000, 12000, 15000};
+    int jogos = 5;
+
+double somaIngressos = 0;
+
+for(int i = 0; i < jogos; i++){
+    somaIngressos += ingressos[i];
+}
+    double media = somaIngressos / jogos;
+
+    std::cout<<"a media de ingressos foi de "<<media<<std::endl;
     return 0;
 }
