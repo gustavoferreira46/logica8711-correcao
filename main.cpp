b6e6cd7d0070193bc19f16165f341ff21e70e8c0
GUSTAVO FERREIRA PEDROSO <03713982074@senacrs.edu.br>
2026-07-16 20:31:21 -0300

    ler array na posição correta e invertida

--- main.cpp
+++ main.cpp
@@ -3,16 +3,17 @@
 
     
 int main(){
-    int numero [10] = {12, 87, 45, 3, 99, 28, 61, 7, 74, 50};
-    int contador = 0;
+    int numeros [8] = {12, 87, 45, 3, 99, 28, 61, 7};
+    
 
-    for(int i = 0; i < 10; i++){
-        if(numero[i] > 50){
-            std::cout<<numero[i]<<" ";
-            contador++;
-        }
+    for(int num:numeros){
+        std::cout<<"posicao correta: "<<num<<std::endl;
+       
     }
-
+    
+    for(int i = 7; i >= 0; i--){
+        std::cout<<"invertido: "<<numeros[i]<<std::endl;
+    }
 
     return 0;
 }
