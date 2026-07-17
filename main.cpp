13e739b498f1e6e58b7cf8d07bce4e1386a0805f
GUSTAVO FERREIRA PEDROSO <03713982074@senacrs.edu.br>
2026-07-16 21:34:37 -0300

    ler 10 numeros e exibir quantas vezes o numero 5 aparece

--- main.cpp
+++ main.cpp
@@ -3,17 +3,19 @@
 
     
 int main(){
-    int numeros [8] = {12, 87, 45, 3, 99, 28, 61, 7};
-    
+    int numeros;
+    int contador = 0;
 
-    for(int num:numeros){
-        std::cout<<"posicao correta: "<<num<<std::endl;
-       
-    }
     
-    for(int i = 7; i >= 0; i--){
-        std::cout<<"invertido: "<<numeros[i]<<std::endl;
+    
+    for(int i = 1; i <= 10; i++){
+        std::cout<<"digite um numero: ";
+        std::cin>>numeros;
+        if(numeros == 5){
+            contador++;
+        }
     }
 
+    std::cout<<"o numero 5 aparece "<<contador<<" vezes no array"<<std::endl;
     return 0;
 }
